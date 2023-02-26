e13efc025d5d0b506c3873aa1c6e72750fce235f
Alexander Shangin <alarriva@yandex.ru>
2023-02-26 23:04:03 +0600

    Add files

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,16 @@
+import argparse
+
+
+parser = argparse.ArgumentParser(prog = 'gendiff',
+                    description = 'Compares two configuration files and shows a difference.',
+                    epilog = '')
+
+parser.add_argument("first_file")
+parser.add_argument("second_file")
+
+args = parser.parse_args()
+
+print(args)
+
+
+
