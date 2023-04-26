c4e860b777f6799016a3bb75651792052cfa55a9
Alexander Shangin <alarriva@yandex.ru>
2023-04-26 21:21:33 +0600

    Fix files

--- tests/test_diff_files.py
+++ tests/test_diff_files.py
@@ -9,10 +9,10 @@ def test_generate_diff():
     filepath2 = os.path.join(os.path.dirname(__file__), 'fixtures', 'file2.json')
     dict1 = parse_file(filepath1)
     dict2 = parse_file(filepath2)
-    # продумать с фикстурами. Вывод реально большой.
-    expected_output = """{\n  - follow: False\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: True\n}""".strip()
-
     
+    with open(os.path.join(os.path.dirname(__file__), 'fixtures', 'expected_output.txt'), 'r') as diff:
+        expected_output = diff.read().strip()
+      
     assert make_diff(dict1, dict2) == expected_output
 
 # import pytest
